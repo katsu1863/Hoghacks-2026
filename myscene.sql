b409b4ead9dff48aa7c469d13d0d6196e9ca70a9
slin1863 <sxuelin.1863@gmail.com>
2026-04-03 22:28:33 -0500

    Adding sql file for database schema

---
+++ myscene.sql
@@ -0,0 +1,18 @@
+-- Schema for MyScene MySQL database
+
+CREATE TABLE Artists (
+    id INT PRIMARY KEY,
+    artist_name VARCHAR(40) NOT NULL,
+    location_city VARCHAR(40) NOT NULL,
+    location_region VARCHAR(40) NOT NULL,
+    music_genre VARCHAR(20) NOT NULL,
+    insta_handle VARCHAR(40),
+    image_src VARCHAR(40)
+);
+
+CREATE TABLE Users (
+    username VARCHAR(10) NOT NULL,
+    user_password VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (username, user_password)
+);
