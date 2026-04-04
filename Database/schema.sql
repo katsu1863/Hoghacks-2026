3ff2678be17532ad4e15f20902bdc5e6013ede58
slin1863 <sxuelin.1863@gmail.com>
2026-04-04 01:29:51 -0500

    Testing verify_user.php

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,13 +1,18 @@
 -- Schema for MyScene MySQL database
 
-CREATE DATABASE MyScene;
+CREATE DATABASE IF NOT EXISTS MyScene;
 USE MyScene;
+
+DROP TABLE IF EXISTS Artists;
+DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Artists (
     id INT PRIMARY KEY,
     artist_name VARCHAR(40) NOT NULL,
     location_city VARCHAR(40) NOT NULL,
     location_region VARCHAR(40) NOT NULL,
+    longitude FLOAT NOT NULL,
+    latitude FLOAT NOT NULL,
     music_genre VARCHAR(20) NOT NULL,
     insta_handle VARCHAR(40),
     image_src VARCHAR(40)
--- login.js
+++ login.js
@@ -1,17 +1,19 @@
 document.getElementById("login").addEventListener("click", function() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+    // var username = document.getElementById("username").value;
+    // var password = document.getElementById("password").value;
 
     let accepted = false;
 
-    fetch("login.php", {
+    fetch("verify_user.php", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            username: username,
-            password: password
+            // username: username,
+            // password: password
+            username: "mrpepsi",
+            password: "12345"
         })
     }).then(response => response.json())
       .then(data => {
@@ -20,7 +22,8 @@ document.getElementById("login").addEventListener("click", function() {
           //redirect to main page
           //else show error message
           if (accepted) {
-              window.location.href = "main.html";
+              // window.location.href = "main.html";
+              alert("Login successful")
           } else {
               alert("Invalid username or password");
           }
