bf035755b6fde30f6cb154a787a926303ff9c609
slin1863 <sxuelin.1863@gmail.com>
2026-04-04 10:19:13 -0500

    Updated schema.sql and implement add_artist.php

--- schema.sql
+++ schema.sql
@@ -6,12 +6,12 @@ DROP TABLE IF EXISTS Artists;
 DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Artists (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     artist_name VARCHAR(40) NOT NULL,
     location_city VARCHAR(40) NOT NULL,
     location_region VARCHAR(40) NOT NULL,
-    longitude FLOAT NOT NULL,
-    latitude FLOAT NOT NULL,
+    longitude FLOAT,
+    latitude FLOAT,
     music_genre VARCHAR(20) NOT NULL,
     insta_handle VARCHAR(40),
     image_src VARCHAR(40)
