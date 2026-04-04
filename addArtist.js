cba4278da0035c2e12260e55f43180f053be36d2
Kawaiikittykatt <46823786+Kawaiikittykatt@users.noreply.github.com>
2026-04-04 01:55:12 -0500

    Update addArtist.js
    lat & long

--- addArtist.js
+++ addArtist.js
@@ -4,6 +4,9 @@ document.getElementById("addArtist").addEventListener("click", function() {
     var genre = document.getElementById("Genre").value;
     var description = document.getElementById("Description").value;
     var image = document.getElementById("Image").value;
+    var socials = document.getElementById("Socials").value;
+    var date = document.getElementById("Dates").value;
+
 
     fetch("addArtist.php", {
         method: "POST",
@@ -12,16 +15,25 @@ document.getElementById("addArtist").addEventListener("click", function() {
         },
         body: JSON.stringify({
             name: name,
+            longitude: getLongitude(location),
+            latitude: getLatitude(location),
             location: location,
             genre: genre,
             description: description,
-            image: image
+            image: image,
+            socials: socials,
+            date: date
+
         })
     });
 
     name.value = "";
+    longitude.value = "";
+    latitude.value = "";
     location.value = "";
     genre.value = "";
     description.value = "";
     image.value = "";
+    socials.value = "";
+    date.value = "";
 });
